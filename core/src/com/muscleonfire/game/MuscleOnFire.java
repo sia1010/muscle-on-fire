37d3fb25df4bdc1ce4f2a3fb8e3065cd21418e02
Jocelyn <hui_21001427@utp.edu.my>
2023-01-31 17:27:04 +0800

    jocelyn write comment

--- core/src/com/muscleonfire/game/MuscleOnFire.java
+++ core/src/com/muscleonfire/game/MuscleOnFire.java
@@ -13,6 +13,7 @@ public class MuscleOnFire extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		// hihihihihih
 	}
 
 	@Override
