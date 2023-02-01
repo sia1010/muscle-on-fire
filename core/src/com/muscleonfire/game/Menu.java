aeeef5cc92933a26e87dea6b43f35469d67be24e
sia1010 <sia_21001570@utp.edu.my>
2023-02-01 16:24:11 +0800

    changed resolution to portrait

--- core/src/com/muscleonfire/game/Menu.java
+++ core/src/com/muscleonfire/game/Menu.java
@@ -1,13 +1,19 @@
 package com.muscleonfire.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Menu implements Screen {
     final MuscleOnFire game;
+    OrthographicCamera camera;
 
     public Menu(final MuscleOnFire game) {
         this.game = game;
-        game.setScreen(new GameScreen(this.game));
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 480, 800);
         //hi
     }
 
@@ -18,7 +24,11 @@ public class Menu implements Screen {
 
     @Override
     public void render(float delta) {
-
+        ScreenUtils.clear(1,1,1,1);
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        game.batch.draw(new Texture(Gdx.files.internal("img.png")), 200, 400);
+        game.batch.end();
     }
 
     @Override
--- desktop/src/com/muscleonfire/game/DesktopLauncher.java
+++ desktop/src/com/muscleonfire/game/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setWindowedMode(540, 960);
 		config.setTitle("muscle-on-fire");
 		new Lwjgl3Application(new MuscleOnFire(), config);
 	}
