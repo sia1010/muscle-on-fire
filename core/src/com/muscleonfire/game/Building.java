f6ff449848ba64a7fc41db24b2a5202216bdea77
Jocelyn <hui_21001427@utp.edu.my>
2023-02-04 22:44:22 +0800

    Game over screen patrick cant move beyond the screen

--- core/src/com/muscleonfire/game/Building.java
+++ core/src/com/muscleonfire/game/Building.java
@@ -9,8 +9,7 @@ public class Building extends GameObject{
     void spawn(){
 
         object = new Rectangle();
-        object.height=0;
-        object.width=0;
+
         object.x=0;
         object.y=0;
         image=new Texture(Gdx.files.internal("background1.png"));
